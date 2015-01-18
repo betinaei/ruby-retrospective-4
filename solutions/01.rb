ee3babeb66bc32add911478a3ae9721698437467
Betina Ivanova <bettina.ivanova@gmail.com>
2015-01-18 23:19:16 +0200

    Add old solution to taks 1

---
+++ solutions/01.rb
@@ -0,0 +1,23 @@
+def series(series_name, index)
+  case series_name
+    when 'fibonacci' then fibonacci(index)
+    when 'lucas' then lucas(index)
+    else summed(index)
+  end
+end
+
+def fibonacci(index)
+  (index == 1 or index == 2) ? 1 : fibonacci(index - 1) + fibonacci(index - 2)
+end
+
+def lucas(index)
+  case index
+    when 1 then 2
+    when 2 then 1
+    else lucas(index - 1) + lucas(index - 2)
+  end
+end
+
+def summed(index)
+  fibonacci(index) + lucas(index)
+end
